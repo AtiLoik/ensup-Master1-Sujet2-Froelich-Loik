f2e92bbe939394a022bce75e5b6dd14c40426901
unknown <loikfr@live.fr>
2019-11-08 15:05:42 +0100

    Création des classes

---
+++ src/Metier/Annonce.java
@@ -0,0 +1,9 @@
+package Metier;
+
+public class Annonce {
+	private int id;
+	private String intitule;
+	private String detail;
+	private String prix;
+
+}
---
+++ src/Metier/Personne.java
@@ -0,0 +1,13 @@
+package Metier;
+
+import java.util.Date;
+
+public class Personne {
+	private int id;
+	private String nom;
+	private String prenom;
+	private String login;
+	private String MotDePasse;
+	private Date DateNaissance;
+
+}
